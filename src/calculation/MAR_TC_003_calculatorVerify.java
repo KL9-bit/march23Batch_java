bb146cb17a76725b060bdcf4bb44d064a0bc525f
KL9-bit <komalclagade@gmail.com>
2024-08-22 18:15:23 +0530

    [komal]check the code added new class which is calculator verify.

---
+++ src/calculation/MAR_TC_003_calculatorVerify.java
@@ -0,0 +1,14 @@
+package calculation;
+
+public class MAR_TC_003_calculatorVerify {
+	public static void main(String[] args) {
+		
+		public void calculatorVerify() {
+			
+			
+		System.out.println("calculator is verified");
+		
+		}
+	}
+
+}
